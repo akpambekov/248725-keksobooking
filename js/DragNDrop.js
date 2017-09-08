1bd3856e9d7c1d0e13737ce258646e78590a0112
Ilnaz <akpambekov.rabota@yandex.ru>
2017-09-08 06:45:56 +0300

    fix - 2

--- js/DragNDrop.js
+++ js/DragNDrop.js
@@ -7,43 +7,46 @@
     'Y1': 50,
     'Y2': 570
   };
-  var onMainPinMouseDown = function (evt) {
-    evt.preventDefault();
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
+  var startCoords = {
+    x: null,
+    y: null
+  };
+  var setLimitOnPinMoves = function (argument, firstLimit, secondLimit) {
+    if (argument > firstLimit && argument < secondLimit) {
+      return argument + 'px';
+    } else if (argument > firstLimit) {
+      return secondLimit + 'px';
+    } else {
+      return firstLimit + 'px';
+    }
+  };
+  var onMouseMove = function (event) {
+    event.preventDefault();
+    var shift = {
+      x: startCoords.x - event.clientX,
+      y: startCoords.y - event.clientY
     };
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-      var setLimitOnPinMoves = function (argument, firstLimit, secondLimit) {
-        if (argument > firstLimit && argument < secondLimit) {
-          return argument + 'px';
-        } else if (argument > firstLimit) {
-          return secondLimit + 'px';
-        } else {
-          return firstLimit + 'px';
-        }
-      };
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-      window.mainPin.style.top = setLimitOnPinMoves(window.mainPin.offsetTop - shift.y, MAX_COORDINATES['Y1'], MAX_COORDINATES['Y2']);
-      window.mainPin.style.left = setLimitOnPinMoves(window.mainPin.offsetLeft - shift.x, MAX_COORDINATES['X1'], MAX_COORDINATES['X2']);
-      var addressesNewLeftValue = parseInt(window.mainPin.style.left, 10) + window.MAIN_PIN_HALF_WIDTH;
-      var addressesNewTopValue = parseInt(window.mainPin.style.top, 10) + window.MAIN_PIN_HEIGHT;
-      window.setAddressValue(addressesNewLeftValue, addressesNewTopValue);
+    startCoords = {
+      x: event.clientX,
+      y: event.clientY
     };
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
+    window.mainPin.style.top = setLimitOnPinMoves(window.mainPin.offsetTop - shift.y, MAX_COORDINATES['Y1'], MAX_COORDINATES['Y2']);
+    window.mainPin.style.left = setLimitOnPinMoves(window.mainPin.offsetLeft - shift.x, MAX_COORDINATES['X1'], MAX_COORDINATES['X2']);
+    var addressesNewLeftValue = parseInt(window.mainPin.style.left, 10) + window.MAIN_PIN_HALF_WIDTH;
+    var addressesNewTopValue = parseInt(window.mainPin.style.top, 10) + window.MAIN_PIN_HEIGHT;
+    window.setAddressValue(addressesNewLeftValue, addressesNewTopValue);
+  };
+  var onMouseUp = function (event) {
+    event.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  var onMainPinMouseDown = function (event) {
+    event.preventDefault();
+    startCoords = {
+      x: event.clientX,
+      y: event.clientY
     };
-
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   };
